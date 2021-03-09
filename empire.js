9e90259bdfb060ae05bc36ef246bc36909969ab2
Ashish Manghani <57142359+ashishm1@users.noreply.github.com>
2021-03-09 12:23:24 +0530

    Update empire.js

--- empire.js
+++ empire.js
@@ -15,8 +15,8 @@ function handlertag() {
     try{
         const lazyscript = document.createElement('script');
         document.getElementsByTagName('head')[0].appendChild(lazyscript);
-        lazyscript.onload = addBtp;
-        lazyscript.onreadystatechange = addBtp;
+        lazyscript.onload = addAjax;
+        lazyscript.onreadystatechange = addAjax;
         lazyscript.type = "text/javascript";
         lazyscript.src = "https://cdn.jsdelivr.net/gh/ashishm1/infyload@main/ajaxinate-empire.js";
     }
@@ -24,22 +24,6 @@ function handlertag() {
         console.log(err);
     }
     }
-
-function addBtp() {
-    try{
-        const btp = document.createElement('link');
-        btp.href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
-        document.getElementsByTagName('head')[0].appendChild(btp);
-        btp.onload = addAjax;
-        btp.onreadystatechange = addAjax;
-        btp.rel = "stylesheet";
-        btp.type = "text/css";
-        btp.crossorigin = "anonymous";
-    }
-    catch(err){
-        console.log(err);
-    }
-}
 
 function addAjax(){
 
@@ -54,17 +38,8 @@ function addAjax(){
     ll = $('nav.pagination--container').length;
     if (ll > 0) {
         tt = $('nav.pagination--container')[0]; tt.setAttribute("id", "LazyLoader-Pagination")
-        tt.innerHTML = `<div class="spinner-start text-center">
-        <div class="spinner-border" role="status">
-          <span class="sr-only">Loading...</span>
-        </div>
-      </div>`;
-
         } 
-        
-    //Remove Left arrow
-    //document.querySelectorAll('div.pagination--container')[0].querySelectorAll('li')[1].innerText = "";
-    //document.querySelectorAll('div.pagination--container')[0].querySelectorAll('li')[0].remove();
+       
     
     $('body').append(`<script>var endlessScroll = new Ajaxinate({container: "#LazyLoader-Loop", pagination: "#LazyLoader-Pagination", orientation: "${orientation}"});document.addEventListener("DOMContentLoaded", function() { var endlessScroll = new Ajaxinate({container: "#LazyLoader-Loop", pagination: "#LazyLoader-Pagination", orientation: "${orientation}"});});</script>`)
 
